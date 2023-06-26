42ec4f6cc0b1a7b993d383af97fcf9b5dbbce2fe
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-06-26 01:09:39 -0600

    depend on :jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,6 +106,8 @@ publishing {
             artifactId = project.properties["archives_base_name"] as String? ?: project.name
             version = project.version as String
 
+            from(components["java"])
+
             artifact(tasks["shadowJar"]) {}
         }
     }
